a452724e66e3902bab23005c953d89970ac13647
AniqMurad <nathaaniq0506@gmail.com>
2023-09-26 16:00:39 +0500

    Video 0 full

--- src/components/Trusted.js
+++ src/components/Trusted.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Trusted = () => {
   return (
-    <Wrapper>
-      <div></div>
+    <Wrapper className="brand-section">
+      <div className="container">
+        <h3>Trusted By 1000+ Companies</h3>
+        <div className="brand-section-slider">
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="slide">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"
+              alt="trusted-brands"
+            />
+          </div>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
 const Wrapper = styled.section`
   padding: 9rem 0;
